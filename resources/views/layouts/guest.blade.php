33049e00557ecf50988f8a807a1e338f809a6b5e
Mediansyah Malik <julianto_rpl24@hotmail.com>
2019-12-13 10:42:07 +0700

    median update cek_pendaftaran

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -42,7 +42,7 @@ The above copyright notice and this permission notice shall be included in all c
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ config('app.name', 'Home') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
